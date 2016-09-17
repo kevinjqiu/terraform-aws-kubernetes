53f06bacb2ce6ce8f4963280e8ac9aabcbf34c86
Kevin Jing Qiu <kevin@idempotent.ca>
2016-09-17 16:34:43 -0400

    Moved keys and certs out of terraform

--- terraform/instances.tf
+++ terraform/instances.tf
@@ -11,15 +11,6 @@ resource "aws_instance" "etcd" {
     tags {
         Name = "kubernetes-etcd-${count.index}"
     }
-    provisioner "file" {
-        source      = "certs/"
-        destination = "/home/ubuntu"
-        connection {
-            type        = "ssh"
-            user        = "ubuntu"
-            private_key = "${file("keys/id_rsa")}"
-        }
-    }
 }
 
 resource "aws_instance" "kube_controller" {
@@ -35,15 +26,6 @@ resource "aws_instance" "kube_controller" {
     tags {
         Name = "kubernetes-controller-${count.index}"
     }
-    provisioner "file" {
-        source      = "certs/"
-        destination = "/home/ubuntu"
-        connection {
-            type        = "ssh"
-            user        = "ubuntu"
-            private_key = "${file("keys/id_rsa")}"
-        }
-    }
 }
 
 resource "aws_instance" "kube_worker" {
@@ -58,14 +40,5 @@ resource "aws_instance" "kube_worker" {
     iam_instance_profile        = "${aws_iam_instance_profile.kube.id}"
     tags {
         Name = "kubernetes-worker-${count.index}"
-    }
-    provisioner "file" {
-        source      = "certs/"
-        destination = "/home/ubuntu"
-        connection {
-            type        = "ssh"
-            user        = "ubuntu"
-            private_key = "${file("keys/id_rsa")}"
-        }
     }
 }
--- terraform/keys.tf
+++ terraform/keys.tf
@@ -1,4 +1,4 @@
 resource "aws_key_pair" "kube" {
     key_name = "kubernetes"
-    public_key = "${file("keys/id_rsa.pub")}"
+    public_key = "${file("../keys/id_rsa.pub")}"
 }
