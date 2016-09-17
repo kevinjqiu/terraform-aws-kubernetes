1cf301119f997dccb199b9d9d564abe40b178897
Kevin Jing Qiu <kevin@idempotent.ca>
2016-09-17 17:49:27 -0400

    Allow egress

--- terraform/security.tf
+++ terraform/security.tf
@@ -24,6 +24,13 @@ resource "aws_security_group" "kube" {
         cidr_blocks = ["0.0.0.0/0"]
     }
 
+    egress {
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
     tags {
         Name = "kubernetes"
     }
