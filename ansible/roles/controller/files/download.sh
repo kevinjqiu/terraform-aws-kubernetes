bc76cc5ab70e66bc1b593f97431c9742b231eabf
Kevin Jing Qiu <kevin@idempotent.ca>
2016-09-18 11:40:34 -0400

    Add kube service systemd files

---
+++ ansible/roles/controller/files/download.sh
@@ -0,0 +1,5 @@
+wget https://storage.googleapis.com/kubernetes-release/release/v1.3.6/bin/linux/amd64/kube-apiserver
+wget https://storage.googleapis.com/kubernetes-release/release/v1.3.6/bin/linux/amd64/kube-controller-manager
+wget https://storage.googleapis.com/kubernetes-release/release/v1.3.6/bin/linux/amd64/kube-scheduler
+wget https://storage.googleapis.com/kubernetes-release/release/v1.3.6/bin/linux/amd64/kubectl 
+chmod +x kube*
