e92a56b5d420d706b9f8f8139dd9f7d7cfb53619
Kevin Jing Qiu <kevin@idempotent.ca>
2016-09-18 20:58:31 -0400

    register elb instances

--- terraform/elb.tf
+++ terraform/elb.tf
@@ -3,6 +3,7 @@ resource "aws_elb" "kube" {
 
     security_groups = ["${aws_security_group.kube.id}"]
     subnets         = ["${aws_subnet.kube.id}"]
+    instances       = ["${aws_instance.kube_controller.*.id}"]
     listener {
         instance_protocol  = "TCP"
         instance_port      = 6443
