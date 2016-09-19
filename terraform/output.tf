147164c4e8ab8466e8ad7964934f8b7eb758cfc8
Kevin Jing Qiu <kevin@idempotent.ca>
2016-09-18 21:22:52 -0400

    Add the public name of the ELB to the cert

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,7 +1,8 @@
 output "ips" {
     value = "${jsonencode(concat(aws_instance.etcd.*.public_ip, aws_instance.etcd.*.private_ip,
                                  aws_instance.kube_controller.*.public_ip, aws_instance.kube_controller.*.private_ip,
-                                 aws_instance.kube_worker.*.public_ip, aws_instance.kube_worker.*.private_ip))}"
+                                 aws_instance.kube_worker.*.public_ip, aws_instance.kube_worker.*.private_ip,
+                                 aws_elb.kube.*.dns_name))}"
 }
 
 output "public_etcd_ips" {
@@ -26,4 +27,8 @@ output "public_kube_worker_ips" {
 
 output "private_kube_worker_ips" {
     value = ["${aws_instance.kube_worker.*.private_ip}"]
+}
+
+output "api_server_elb" {
+    value = "${aws_elb.kube.dns_name}"
 }
